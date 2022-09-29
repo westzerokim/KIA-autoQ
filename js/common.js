8f6f152fbef66ed50ba6ed99dbadd237ff357be3
westzerokim <lovetb22@gmail.com>
2022-09-29 13:43:53 +0900

    가로버전 최종
    퍼블리싱 완료

--- js/common.js
+++ js/common.js
@@ -29,3 +29,44 @@ $(".btnBlueGreen").click(function(){
     btn_num = this.innerHTML;
   }
 });
+
+
+$(".fix_history>a").click(function(){  
+  $("#fhi_menu").addClass('on'); 
+  $(".backg").addClass('on'); 
+});
+$(".back_num").click(function(){  
+$("#fhi_menu").removeClass('on');
+$(".backg").removeClass('on');
+});
+
+function getCheckboxValue()  {
+
+
+  const query = 'input[name="fix"]:checked';
+
+  const selectedEls = 
+
+      document.querySelectorAll(query);
+
+  
+
+  let result = '';
+
+  selectedEls.forEach((el) => {
+
+    result += el.value + ' ';
+
+  });
+
+  
+
+  document.getElementById('result').innerText
+
+    = result;
+
+}
+$(".request").click(function(){  
+  $("#popup_wrap").addClass('show'); 
+});
+
